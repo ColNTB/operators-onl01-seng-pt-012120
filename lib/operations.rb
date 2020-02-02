ef54b44a9338a7386913d69db1401099f944720e
Nathan Bohn <nathantbohn@gmail.com>
2020-02-02 22:31:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,7 +12,7 @@ end
 
 def not_safe?(speed)
 	nsafe = true
-	speed > 60 ? nsafe = true: nsafe = false
+	#speed > 60 ? nsafe = true: nsafe = false
 	speed < 40 ? nsafe = true : speed > 60 ? nsafe = true : nsafe = false 
 	return nsafe 
 end
